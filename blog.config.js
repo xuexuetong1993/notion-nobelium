f405d11950a342d3416b8a0f7003436246ed704a
xuexuetong1993 <995135827@qq.com>
2022-04-17 01:33:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: true, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://baidu.com',
   seo: {
     keywords: ['Blog', 'Website', 'xuetong'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
